736d91c9713446ef5444678150dab5fe1053ec0e
leewebstechnologies <aliuajibade317@gmail.com>
2024-03-13 11:47:25 +0100

    ui completed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,12 +1,19 @@
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
-        <img src="" alt="" />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1663957826118-23438626bc8d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmlnZXJpYW4lMjBtYW58ZW58MHx8MHx8fDA%3D"
+          alt=""
+        />
         <span>Just Now</span>
       </div>
       <div className="messageContent">
         <p>Hello</p>
+        <img
+          src="https://media.istockphoto.com/id/517302398/photo/portrait-of-nigerian-man-with-beard-looking-at-camera.jpg?s=612x612&w=0&k=20&c=BC5pdsmTWzmFO3mIlA7TQAIECnJ7Kpd-daL6G4RJqT4="
+          alt=""
+        />
       </div>
     </div>
   );
